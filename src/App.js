5496474cfe230f022f93b3b497a01d8b4392db4f
Maireen Espiritu <maireenespiritu@dhcp-v003-105.mobile.uci.edu>
2018-11-17 11:41:15 -0800

    centered text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Button, Segment, Header } from 'semantic-ui-react'
+import { Header, Icon, Image, Button } from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
+
+// const app = () => (
+//   <div>
+//     <Header as='h1' icon textAlign='center'>
+//       <Icon name='thumbs up outline' circular />
+//       <Header.Content>Meme Translator</Header.Content>
+//     </Header>
+//     <Button basic color='black' content='Black'/>
+//   </div>
+ 
+
+// );
+
+// export default app
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Segment clearing>
-          <Header as='h1' color='blue' floated='left'>Meme</Header>
-        </Segment>
-      </div>
+       <Header as='h1' icon textAlign='center'>
+         <Icon name='thumbs up outline' circular />
+         <Header.Content>Meme Translator</Header.Content>
+     </Header>
+     <Button class='button' basic color='black' content='Translate me!'/>
+   </div>
     );
   }
 }
